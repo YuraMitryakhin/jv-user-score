2df5d3e465c95d3726efa089e1c559cef5e45c34
Yura <yuramitryahin@gmail.com>
2020-12-03 15:52:08 +0200

    created class exception and has done task in method getUserScore

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+        String[] valuesEmailAndScore;
+        for (String record : records) {
+            valuesEmailAndScore = record.split(":");
+            if (valuesEmailAndScore[0].equals(email)) {
+                return Integer.parseInt(valuesEmailAndScore[1]);
+            }
+        }
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
--- src/main/java/core/basesyntax/exception/UserNotFoundException.java
+++ src/main/java/core/basesyntax/exception/UserNotFoundException.java
@@ -1,3 +1,5 @@
+package core.basesyntax.exception;
+
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
